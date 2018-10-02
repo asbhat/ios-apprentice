6d361b17961cd3e548ccc9c0a908efe40f0e163b
Aditya <3885798+asbhat@users.noreply.github.com>
2018-10-01 22:38:07 -0700

    better looking BullsEye

---
+++ BullsEye/BullsEye/InfoViewController.swift
@@ -0,0 +1,55 @@
+//
+//  InfoViewController.swift
+//  BullsEye
+//
+//  Created by Aditya Bhat on 10/1/18.
+//  Copyright © 2018 Aditya Bhat. All rights reserved.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import UIKit
+import WebKit
+
+class InfoViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html") {
+            if let htmlData = try? Data(contentsOf: url) {
+                let baseURL = URL(fileURLWithPath: Bundle.main.bundlePath)
+                webView.load(htmlData, mimeType: "text/html", characterEncodingName: "UTF-8", baseURL: baseURL)
+            }
+        }
+    }
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
