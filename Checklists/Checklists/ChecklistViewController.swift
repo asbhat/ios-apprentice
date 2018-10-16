29d9a86185e93dfd31ff69c0b8f9fbabc0677e27
Aditya <3885798+asbhat@users.noreply.github.com>
2018-10-15 22:39:13 -0700

    sample row data with selection

--- Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -16,14 +16,38 @@ class ChecklistViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 100
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let checklistItemId = "ChecklistItem"
         let cell = tableView.dequeueReusableCell(withIdentifier: checklistItemId, for: indexPath)
+        let label = cell.viewWithTag(1000) as! UILabel
+
+        switch (indexPath.row % 5) {
+        case 0:
+            label.text = "Walk the dog"
+        case 1:
+            label.text = "Brush my teeth"
+        case 2:
+            label.text = "Learn iOS development"
+        case 3:
+            label.text = "Soccer practice"
+        case 4:
+            label.text = "Eat ice cream"
+        default:
+            break
+        }
 
         return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = (cell.accessoryType == .none) ? .checkmark : .none
+        }
+
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 }
